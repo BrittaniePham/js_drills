06b59eec137d34828ecbd1beca4d5b0f74bc8121
Brittanie Pham <brittaniepham@gmail.com>
2018-05-16 23:21:20 -0600

    drill 4 (vowel counter)

---
+++ test4.js
@@ -0,0 +1,16 @@
+// 4 Vowel Count --------------------
+function vowelCount(string){
+  var vowels = ['a','e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+  var count = 0
+  var array = string.split("")
+  for(var i = 0; i < array.length; i++) {
+    if (vowels.includes(array[i])) {
+      count++
+    }
+  } 
+  console.log(count)
+}
+
+var sentence = "I'm sleepy all of the time"
+
+vowelCount(sentence)
